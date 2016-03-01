74a1d162b38abfe8fbe2ed96ec5ce8bc9405fab6
vharr11 <vharr11@lsu.edu>
2016-02-29 21:27:13 -0600

    Added files via upload

--- Demo/js/script.js
+++ Demo/js/script.js
@@ -1,24 +1,21 @@
-/* Comment: This variable adds a basemap and specifies
-the location the map will focus on and the level of zoom
-the map will load at */
+/* Comment: This variable adds a map object and specifies
+the geographic location  the map will start at and the zoom level */
 var map = L.map('map').setView([34.055448, -90.182421], 4);
 
-/* This tilelayer specifies which leaflet map will be used,
-which includes some metadata about the map creation and the
-maximum zoom level available for the map */
+/* This tilelayer specifies the leaflet basemap,and includes
+ metadata about the map creation (attribution) and the maximum zoom level.
+ The .addTo(map) commmand adds this leaflet basemap to the previously specified
+ map object */
 L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
 	maxZoom: 16
 }).addTo(map);
 
-/* This variable adds a tilelayer of WMS. Specified within
-this section of text is the layer number of the specific aspect
-of the WMS data to be displayed. The format of the tilelayer is
-specified, and in this case is an image. Transparency is specified as
-either true (possible) or false (not possible), and the attribution
-information refers to (in this case) the source of the data. The .addTo(map)
-command at the end adds this variable information to the map already specified
-in the first variable.*/
+/* This variable adds a WMS tilelayer for lightning. "layer" specifies the
+aspect of the WMS data to be displayed. "Format" specifies  the type of data,
+in this case "image/png". Transparency is specified as either true (transparent)
+or false (opaque). "Attribution" information refers to metadata. The .addTo(map)
+command adds this variable to the previously specified map object */
 var lightning = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer",{
 	layers: 1,
 	format: 'image/png',
@@ -26,14 +23,11 @@ var lightning = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoa
 	attribution: "NOAA"
 }).addTo(map);
 
-/* This variable adds a tilelayer of WMS. Specified within
-this section of text is the layer name of the specific aspect
-of the WMS data to be displayed. The format of the tilelayer is
-specified, and in this case is an image. Transparency is specified as
-either true (possible) or false (not possible), and the attribution
-information refers to (in this case) the source of the data. The .addTo(map)
-command at the end adds this variable information to the map already specified
-in the first variable.*/
+/* This variable adds a WMS tilelayer for radar. "layer" specifies the
+aspect of the WMS data to be displayed. "Format" specifies  the type of data,
+in this case "image/[ng]". Transparency is specified as either true (transparent)
+or false (opaque). "Attribution" information refers to metadata. The .addTo(map)
+command adds this variable to the previously specified map object */
 var radar = L.tileLayer.wms("http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi",{
 	layers: 'nexrad-n0r-900913',
 	format: 'image/png',
@@ -41,14 +35,12 @@ var radar = L.tileLayer.wms("http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad
 	attribution: "Weather data © 2012 IEM Nexrad"
 }).addTo(map);
 
-/* This variable adds a tilelayer of WMS. Specified within
-this section of text is the layer number of the specific aspect
-of the WMS data to be displayed. The format of the tilelayer is
-specified, and in this case is an image. Transparency is specified as
-either true (possible) or false (not possible), and the attribution
-information refers to (in this case) the source of the data. The .addTo(map)
-command at the end adds this variable information to the map already specified
-in the first variable.*/
+/* This variable adds a WMS tilelayer for temperatures. "layer" specifies the
+aspect of the WMS data to be displayed. "Format" specifies  the type of data,
+in this case "image/png". Transparency is specified as either true (transparent)
+or false (opaque). "Attribution" information refers to metadata. "Opacity" specifies
+how transparent the tilelayer will be. The .addTo(map) command adds this variable to
+the previously specified map object */
 var temperature = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/forecast_meteoceanhydro_sfc_ndfd_dailymaxairtemp_offsets/MapServer/WMSServer",{
 	layers: 1,
   format: 'image/png',
@@ -57,7 +49,7 @@ var temperature = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowc
 	opacity: 0.30
 }).addTo(map);
 
-/* This section of code creates a removabale overlay for each WMS tilelayer.
+/* This varable creates a removabale overlay for each WMS tilelayer.
 Overlay names should match the tilelayer name.*/
 var overlays ={
 	"Lightning": lightning,
@@ -65,5 +57,7 @@ var overlays ={
 	"Temperature":temperature
 };
 
-// This line of code adds the layer overlays to the basemap.
+/* This line of code adds the layer overlays to the basemap.
+{} is required as the first item in the array for the overlays
+to reference */
 L.control.layers({}, overlays).addTo(map)
